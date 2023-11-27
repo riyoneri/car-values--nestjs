4d5f197bf689e9656690540a3f178746f7272033
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-27 11:15:14 +0200

    fix: imports of report entity in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
