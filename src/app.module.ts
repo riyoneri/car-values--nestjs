8864f0e86e7f9ad7279d1a00cfd5df0e522d3b5b
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-07 17:45:24 +0200

    feat: split files into users and reports sections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
 })
 export class AppModule {}
