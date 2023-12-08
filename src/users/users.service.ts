584df4054dccbdf7a17c67db3d73fda207379220
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-08 18:47:06 +0200

    feat: add findOne method in user entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,20 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  update() {}
+
+  remove() {}
 }
