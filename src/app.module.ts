fa4161601a5e81ee81f084b705a03a4bcbe957b8
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-08 13:36:17 +0200

    feat: create auth/signup controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
+// import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [
@@ -17,6 +18,6 @@ import { Report } from './reports/report.entity';
     UsersModule,
     ReportsModule,
   ],
-  controllers: [AppController],
+  controllers: [UsersController],
 })
 export class AppModule {}
