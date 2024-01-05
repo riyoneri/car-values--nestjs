fc1366b3a9780005f60162d049465539ee02f78f
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-05 10:25:48 +0200

    feat: first steps of createReport service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dto/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto) {
+    return this.repo.create(reportDto);
+  }
+}
