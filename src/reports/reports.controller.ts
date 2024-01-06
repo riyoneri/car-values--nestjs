50e156f0d62d5156143b8d12424aa872d425e997
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-06 08:37:39 +0200

    feat: serialize outgoing report with user property

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,13 +2,18 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { ReportsService } from './reports.service';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dto/report';
 
 @Controller('reports')
 @UseGuards(AuthGuard)
+@Serialize(ReportDto)
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
